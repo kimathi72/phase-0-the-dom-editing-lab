134150597cdc36f9471807c8d2b85969a2898732
roy <kimathiwaweru@gmail.com>
2023-03-04 01:06:09 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function renderHTML ()
+// function renderHTML ()
 
-{
-    let body = document.querySelector("body");
-    let heading = document.createElement("h1");
-    let paraGraph = document.createElement("p");
-    let table = document.createElement("table");
-    heading.innerHTML = `My HTML adventure`
-    paraGraph.innerHTML = `<strong>strong text</strong>\n
-    <em></em>\n
-    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">this is a link<a>
-    `
-    body.appendChild(heading)
-    body.appendChild(paraGraph)
-    body.appendChild(table)
-}
+// {
+//     let body = document.querySelector("body");
+//     let heading = document.createElement("h1");
+//     let paraGraph = document.createElement("p");
+//     let table = document.createElement("table");
+//     heading.innerHTML = `My HTML adventure`
+//     paraGraph.innerHTML = `<strong>strong text</strong>\n
+//     <em></em>\n
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">this is a link<a>
+//     `
+//     body.appendChild(heading)
+//     body.appendChild(paraGraph)
+//     body.appendChild(table)
+// }
 
-renderHTML();
+// renderHTML();
